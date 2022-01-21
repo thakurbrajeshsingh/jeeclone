cca970dc9ddd3be2025d1a51bf46e4d66fccbcf3
Brajesh Singh <40537685+thakurbrajeshsingh@users.noreply.github.com>
2022-01-21 16:56:46 +0530

    Bug 2 fixed

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import ContextProvider from './components/context/ContextProvider';
 
 // components 
 import Navbar from './components/header/Navbar';
+
 import Register from "./components/register/Register";
 
 import LandingPage from "./components/home/LandingPage";
